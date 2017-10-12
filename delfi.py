83d2db4a8d26fec4c181a89f24f03c3f42e61a99
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2017-10-12 16:58:45 +0300

    initial test scraper

---
+++ delfi.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+
+import csv
+from lxml import html
+import requests
+import re
+
+
+url = 'https://www.delfi.lt/gyvenimas/anapus-tikroves/ka-galite-suzinoti-apie-vyra-is-jo-zodiako-zenklo.d?id=75912131&com=1&reg=0&no=0&s=2'
+
+pg = requests.get(url)
+
+
+### Regexes
+
+
+IP_re = re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}')
+date_re = re.compile('\d{4}-\d{1,2}-\d{1,2}')
+time_re = re.compile('\d{2}:\d{2}')
+
+
+### Main loop
+
+
+if pg.status_code == 200:
+    
+    tree = html.fromstring(pg.content.decode('utf-8'))
+    
+    for comment in tree.xpath('//div[@data-post-id]'):
+        
+        new_line= {
+            'IP' : IP_re.findall(comment.xpath('./div[@class="comment-date"]/text()')[0]), 
+            'Date' : date_re.findall(comment.xpath('./div[@class="comment-date"]/text()')[0]),
+            'Time' : time_re.findall(comment.xpath('./div[@class="comment-date"]/text()')[0]),
+            'Name' : comment.xpath('./div[@class="comment-author"]/a/text()')
+            }
+        
+        print(new_line)
+        
