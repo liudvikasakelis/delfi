c05b7d5d2bd9eeefb6b0649dc9a2153da9a9cbfc
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2017-10-25 22:14:36 +0300

    Name is broken and bad

--- delfi.py
+++ delfi.py
@@ -15,19 +15,41 @@ s = 2
 reg = 0
 
 
+### XPATHs
+
+
+XPATH = {
+    'IP' : './div[@class="comment-date"]/text()',
+    'Date' : './div[@class="comment-date"]/text()',
+    'Time' : './div[@class="comment-date"]/text()',
+    'Name' : 'string(./div[@class="comment-author"])',
+    'Comment' : './div/div[@class="comment-content-inner"]/text()'
+    }
+
+
 ### Regexes
 
-regexes = {
+
+regex = {
     'IP' : re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'),
     'Date' : re.compile('\d{4}-\d{1,2}-\d{1,2}'),
     'Time' : re.compile('\d{2}:\d{2}')
 }
 
+
+### Substitutions
+
+
+subs = {
+    'Name' : '[\t\n]',
+    'Comment' : '[\t\n]',
+    }
+    
+    
 ### Main loop
 
 
 url = url + '&com=1' + '&reg=' + str(reg) + '&s=' + str(s) + '&no=0'
-
 
 pg = requests.get(url)
 
@@ -39,14 +61,19 @@ while pg.status_code == 200:
     
     for comment in tree.xpath('//div[@data-post-id!=""]'):
         
-        new_line= {
-            'IP' : IP_re.findall(comment.xpath('./div[@class="comment-date"]/text()')[0])[0], 
-            'Date' : date_re.findall(comment.xpath('./div[@class="comment-date"]/text()')[0])[0],
-            'Time' : time_re.findall(comment.xpath('./div[@class="comment-date"]/text()')[0])[0],
-            'Name' : re.sub('[\n\t]', '', comment.xpath('./div[@class="comment-author"]/text()')[0]),
-            'Comment': re.sub('[\n\t]', '', comment.xpath('./div/div[@class="comment-content-inner"]/text()')[0])
-            }
+        new_line = dict()
         
+        for field in XPATH.keys():
+            try:
+                raw = comment.xpath(XPATH[field])[0]
+                if field in regex.keys():
+                    raw = regex[field].findall(raw)[0]
+                if field in subs.keys():
+                    raw = re.sub(subs[field], '', raw)
+                new_line[field] = raw
+            except:
+                pass
+                
         main_list.append(new_line)
         
     if tree.xpath('//a[@class="comments-pager-arrow-last"]/@href'):
@@ -60,7 +87,7 @@ while pg.status_code == 200:
 
 
 with open(outfile, 'w') as csvf:
-    writer = csv.DictWriter(csvf, fieldnames=new_line.keys())
+    writer = csv.DictWriter(csvf, fieldnames=XPATH.keys())
     writer.writeheader()
     for row in main_list:
         writer.writerow(row)
