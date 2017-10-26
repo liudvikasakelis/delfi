340d54c473fd1a0fa2a511f252e0b3a14499614e
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2017-10-26 22:28:05 +0300

    Working and employs design decisions of the current century

--- delfi.py
+++ delfi.py
@@ -7,23 +7,18 @@ import requests
 import re
 
 
-url = 'https://www.delfi.lt/verslas/verslas/apklause-apie-kainas-ir-algas-tokio-issiskyrimo-dar-nera-buve.d?id=76047145'
-
-outfile = 'z.csv'
-
-s = 2
-reg = 0
-
-
 ### XPATHs
 
 
 XPATH = {
+    'ID' : 'string(./@data-post-id)',
     'IP' : 'string(./div[@class="comment-date"])',
     'Date' : 'string(./div[@class="comment-date"])',
     'Time' : 'string(./div[@class="comment-date"])',
     'Name' : 'string(./div[@class="comment-author"])',
-    'Comment' : 'string(./div/div[@class="comment-content-inner"])'
+    'Comment' : 'string(./div/div[@class="comment-content-inner"])',
+    'Up' : 'string(.//div[@class="comment-votes-up"]//span[@class="comment-votes-count"])',
+    'Down' : 'string(.//div[@class="comment-votes-down"]//span[@class="comment-votes-count"])'
     }
 
 
@@ -46,24 +41,13 @@ subs = {
     }
     
     
-### Main loop
+### Functions
 
 
-url = url + '&com=1' + '&reg=' + str(reg) + '&s=' + str(s) + '&no=0'
-
-pg = requests.get(url)
-
-main_list = list()
-
-while pg.status_code == 200:
+def single_comment_parse(comment):
+    new_line = dict()
     
-    tree = html.fromstring(pg.content.decode('utf-8'))
-    
-    for comment in tree.xpath('//div[@data-post-id!=""]'):
-        
-        new_line = dict()
-        
-        for field in XPATH.keys():
+    for field in XPATH.keys():
             try:
                 raw = comment.xpath(XPATH[field])
                 if field in regex.keys():
@@ -73,22 +57,55 @@ while pg.status_code == 200:
                 new_line[field] = raw
             except:
                 pass
-                
-        main_list.append(new_line)
+    
+    return new_line
+    
+    
+def whole_page_parse(tree):
+    
+    main_list = list()
         
-    if tree.xpath('//a[@class="comments-pager-arrow-last"]/@href'):
-        url = tree.xpath('//a[@class="comments-pager-arrow-last"]/@href')[0]
-    else:
-        break
-    print(url)
+    for comment in tree.xpath('//div[@data-post-id!=""]'):
+        
+        main_list.append(single_comment_parse(comment))
+    
+    return main_list
+    
+
+            
+if __name__ == '__main__':
+
+    url = 'https://www.delfi.lt/news/ringas/lit/m-garbaciauskaite-budriene-o-ka-jei-juozas-teisus.d?id=76158889'
+    s = 2
+    reg = 0
+    url = url + '&com=1' + '&reg=' + str(reg) + '&s=' + str(s) + '&no=0'
+
+    outfile = 'z.csv'
+    
     pg = requests.get(url)
 
-## Writeout
+    main_list = list()
 
+    while pg.status_code == 200:
+    
+        tree = html.fromstring(pg.content.decode('utf-8'))
+        
+        main_list = main_list + whole_page_parse(tree)
+        
+        if tree.xpath('//a[@class="comments-pager-arrow-last"]/@href'):
+            url = tree.xpath('//a[@class="comments-pager-arrow-last"]/@href')[0]
+        else:
+            break
+        
+        print(url)
+    
+        pg = requests.get(url)
 
-with open(outfile, 'w') as csvf:
-    writer = csv.DictWriter(csvf, fieldnames=XPATH.keys())
-    writer.writeheader()
-    for row in main_list:
-        writer.writerow(row)
+    ### Writeout
+
+    with open(outfile, 'w') as csvf:
+        writer = csv.DictWriter(csvf, fieldnames=XPATH.keys())
+        writer.writeheader()
+        for row in main_list:
+            writer.writerow(row)
 
