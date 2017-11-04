62533a684bcf53eaf1a9bc67b152ca007bcfd955
Liudvikas Akelis <liudvikas.akelis@gmail.com>
2017-11-04 09:29:40 +0200

    Moved scraper functionality into callable functions to simplify usage as a library

--- delfi.py
+++ delfi.py
@@ -8,46 +8,46 @@ import requests
 import re
 
 
-### XPATHs
+# XPATHs
 
 
 XPATH = {
-    'ID' : 'string(./@data-post-id)',
-    'IP' : 'string(./div[@class="comment-date"])',
-    'Date' : 'string(./div[@class="comment-date"])',
-    'Time' : 'string(./div[@class="comment-date"])',
-    'Name' : 'string(./div[@class="comment-author"])',
-    'Comment' : 'string(./div/div[@class="comment-content-inner"])',
-    'Up' : 'string(.//div[@class="comment-votes-up"]//span[@class="comment-votes-count"])',
-    'Down' : 'string(.//div[@class="comment-votes-down"]//span[@class="comment-votes-count"])'
+    'ID': 'string(./@data-post-id)',
+    'IP': 'string(./div[@class="comment-date"])',
+    'Date': 'string(./div[@class="comment-date"])',
+    'Time': 'string(./div[@class="comment-date"])',
+    'Name': 'string(./div[@class="comment-author"])',
+    'Comment': 'string(./div/div[@class="comment-content-inner"])',
+    'Up': 'string(.//div[@class="comment-votes-up"]//span[@class="comment-votes-count"])',
+    'Down': 'string(.//div[@class="comment-votes-down"]//span[@class="comment-votes-count"])'
     }
 
 
-### Regexes
+# Regexes
 
 
 regex = {
-    'IP' : re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'),
-    'Date' : re.compile('\d{4}-\d{1,2}-\d{1,2}'),
-    'Time' : re.compile('\d{2}:\d{2}')
+    'IP': re.compile('\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'),
+    'Date': re.compile('\d{4}-\d{1,2}-\d{1,2}'),
+    'Time': re.compile('\d{2}:\d{2}')
 }
 
 
-### Substitutions
+# Substitutions
 
 
 subs = {
-    'Name' : '[\t\n]',
-    'Comment' : '[\t\n]',
+    'Name': '[\t\n]',
+    'Comment': '[\t\n]',
     }
-    
-    
-### Functions
+
+
+# Functions
 
 
 def single_comment_parse(comment):
     new_line = dict()
-    
+
     for field in XPATH.keys():
             try:
                 raw = comment.xpath(XPATH[field])
@@ -58,55 +58,59 @@ def single_comment_parse(comment):
                 new_line[field] = raw
             except:
                 pass
-    
+
     return new_line
-    
-    
+
+
 def whole_page_parse(tree):
+    main_list = list()
+
+    for comment in tree.xpath('//div[@data-post-id!=""]'):
+        main_list.append(single_comment_parse(comment))
+
+    return main_list
+
+
+def next_url(tree):
+    if tree.xpath('//a[@class="comments-pager-arrow-last"]/@href'):
+        return tree.xpath('//a[@class="comments-pager-arrow-last"]/@href')[0]
+    else:
+        return 0
+    
+    
+def single_url_scraper(url):
     
     main_list = list()
-        
-    for comment in tree.xpath('//div[@data-post-id!=""]'):
-        
-        main_list.append(single_comment_parse(comment))
+    
+    page = requests.get(url)
+    while page.status_code == 200:
+        tree = html.fromstring(page.content.decode('utf-8'))
+
+        main_list = main_list + whole_page_parse(tree)
+
+        if next_url(tree):
+            page = requests.get(next_url(tree))
+        else:
+            break
     
     return main_list
-    
-    
+
+
 if __name__ == '__main__':
 
     url = sys.argv[1]
     outfile = re.findall('=(\d+)$', url)[0] + '.csv'
-        
-        
+
     s = 2
     reg = 0
     url = url + '&com=1' + '&reg=' + str(reg) + '&s=' + str(s) + '&no=0'
 
+    data = single_url_scraper(url)
     
-    pg = requests.get(url)
-
-    main_list = list()
-
-    while pg.status_code == 200:
-    
-        tree = html.fromstring(pg.content.decode('utf-8'))
-        
-        main_list = main_list + whole_page_parse(tree)
-        
-        if tree.xpath('//a[@class="comments-pager-arrow-last"]/@href'):
-            url = tree.xpath('//a[@class="comments-pager-arrow-last"]/@href')[0]
-        else:
-            break
-            
-        print(url)
-        pg = requests.get(url)
-
-    ### Writeout
+    # Writeout
 
     with open(outfile, 'w') as csvf:
         writer = csv.DictWriter(csvf, fieldnames=XPATH.keys())
         writer.writeheader()
-        for row in main_list:
+        for row in data:
             writer.writerow(row)
-
